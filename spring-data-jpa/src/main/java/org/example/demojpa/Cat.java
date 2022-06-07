782d417d87c153f3c76a30ceceb467112af65ea7
Eugene A. Kuchin <e_kuchin@mail.ru>
2022-06-07 23:05:53 +0500

    Spring Data JPA

---
+++ spring-data-jpa/src/main/java/org/example/demojpa/Cat.java
@@ -0,0 +1,57 @@
+package org.example.demojpa;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class Cat {
+
+    @GeneratedValue
+    @Id
+    private Long id;
+
+    @Column(name="imya", nullable = true, updatable = true)
+    private String name;
+    private String breed;
+
+    public Cat() {
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", breed='" + breed + '\'' +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public Cat(String name, String breed) {
+        this.name = name;
+        this.breed = breed;
+    }
+}
---
+++ spring-data-jpa/src/main/java/org/example/demojpa/DemoJpaApplication.java
@@ -0,0 +1,46 @@
+package org.example.demojpa;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class DemoJpaApplication implements CommandLineRunner {
+
+	@Autowired
+	CatRepository repoCat;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJpaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello, JPA");
+
+		Cat manul = new Cat ("Мурзик", "Манул");
+		Cat sphynx = new Cat ("Рамзес", "Сфинкс");
+		Cat brit = new Cat ("Эдуард", "Британец");
+
+		repoCat.save(manul);
+		repoCat.save(sphynx);
+		repoCat.save(brit);
+
+		System.out.println("Коты");
+		repoCat.findAll().forEach(System.out::println);
+
+		Cat dbCat = repoCat.findById(1L).orElseThrow();
+
+		dbCat.setName("Барсик");
+		repoCat.save(dbCat);
+
+		repoCat.deleteById(2L);
+
+		System.out.println("Финальные коты");
+		repoCat.findAll().forEach(System.out::println);
+
+	}
+}
